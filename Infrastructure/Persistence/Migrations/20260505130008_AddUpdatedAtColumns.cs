13425c569e935da1dae56513f86d5f3c0393cd11
Rajeesh K V <rajeeshkva2z@gmail.com>
2026-05-05 18:30:29 +0530

    Missed migration script

---
+++ Infrastructure/Persistence/Migrations/20260505130008_AddUpdatedAtColumns.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdatedAtColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "updated_at",
+                table: "tenants",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "updated_at",
+                table: "subscriptions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "updated_at",
+                table: "tenants");
+
+            migrationBuilder.DropColumn(
+                name: "updated_at",
+                table: "subscriptions");
+        }
+    }
+}
